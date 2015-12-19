46633be8f4d6f5d973b81bba48a7103ebbda2e1e
mrkbryn <mabryan.mb@gmail.com>
2015-12-19 16:22:18 -0600

    Initial commit

---
+++ api-example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  api-example
+//
+//  Created by Mark Bryan on 12/19/15.
+//  Copyright © 2015 Mark Bryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
